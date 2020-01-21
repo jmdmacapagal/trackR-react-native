6d71a3829e7717b4b33d44a20219f85a145da88f
jmdmacapagal <jmdmacapagal@gmail.com>
2020-01-22 00:01:17 +0800

    added mongoose settings, nodemon

--- trackR-server/src/index.js
+++ trackR-server/src/index.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://admin:4bpg8a7x@trackr-prqas.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to Atlas!");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to Atlas", err);
+});
 
 app.get("/", (request, response) => {
   response.send("Hello World");
