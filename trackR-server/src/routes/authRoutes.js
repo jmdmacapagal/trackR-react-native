30a171774c6f7ec208b4ba90f435dd1cccfc4886
jmdmacapagal <jmdmacapagal@gmail.com>
2020-01-22 00:32:27 +0800

    added create user

--- trackR-server/src/routes/authRoutes.js
+++ trackR-server/src/routes/authRoutes.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const router = express.Router();
 
-router.post("/sign-up", (request, response) => {
+router.post("/sign-up", async (request, response) => {
+  const { email, password } = request.body;
+
+  const user = new User({ email, password });
+  await user.save();
+
   response.send("You made a request");
 });
 
