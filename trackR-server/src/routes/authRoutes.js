4ac1291cc44872224f7f4b4f0da76913b8b97b28
jmdmacapagal <jmdmacapagal@gmail.com>
2020-01-22 02:26:40 +0800

    added user auth, hasing

--- trackR-server/src/routes/authRoutes.js
+++ trackR-server/src/routes/authRoutes.js
@@ -17,4 +17,28 @@ router.post("/sign-up", async (request, response) => {
   }
 });
 
+router.post("/sign-in", async (request, response) => {
+  const { email, password } = request.body;
+
+  if (!email || !password) {
+    return response
+      .status(422)
+      .send({ error: "Must provide email and password" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return response.status(422).send({ error: "Invalid password or email." });
+  }
+
+  try {
+    await user.comparePassword(password);
+
+    const token = jwt.sign({ userId: user._id }, "YABA_YODA");
+    response.send({ token });
+  } catch (err) {
+    return response.status(422).send({ error: "Invalid password or email." });
+  }
+});
+
 module.exports = router;
