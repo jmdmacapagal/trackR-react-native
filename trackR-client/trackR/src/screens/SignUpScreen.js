47c67bfbe5747d9ee92dd5bbf604d4ed253dbe7e
jmdmacapagal <jmdmacapagal@gmail.com>
2020-01-25 02:33:33 +0800

    navigation setup, buttons and wiring of screens

--- trackR-client/trackR/src/screens/SignUpScreen.js
+++ trackR-client/trackR/src/screens/SignUpScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
   return (
     <View>
       <Text>SignUpScreen</Text>
+      <Button
+        title="Go to Sign in"
+        onPress={() => navigation.navigate("SignIn")}
+      />
+      <Button
+        title="Go to Main Flow"
+        onPress={() => navigation.navigate("mainFlow")}
+      />
     </View>
   );
 };
