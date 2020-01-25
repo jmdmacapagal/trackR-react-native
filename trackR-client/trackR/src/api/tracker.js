ec607798b9ca462a1f86c51970df67ed8f24adf7
jmdmacapagal <jmdmacapagal@gmail.com>
2020-01-26 01:46:14 +0800

    sign up api connection

---
+++ trackR-client/trackR/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://510a87e3.ngrok.io"
+});
--- trackR-client/trackR/src/context/AuthContext.js
+++ trackR-client/trackR/src/context/AuthContext.js
@@ -1,14 +1,39 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "ADD_ERROR":
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
 
+const signUp = dispatch => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post("/sign-up", { email, password });
+      console.log(response.data);
+    } catch (err) {
+      dispatch({
+        type: "ADD_ERROR",
+        payload: "Something went wrong with sign up."
+      });
+    }
+  };
+};
+
+const signIn = dispatch => {
+  return ({ email, password }) => {};
+};
+
+const signOut = dispatch => {
+  return () => {};
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signIn, signOut, signUp },
+  { isSignedIn: false, errorMessage: null }
 );
